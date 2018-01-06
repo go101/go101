96fd3ddb45f75ff46e02f724e2e8b4ca158b6ad6
Go101 <go101@github.com>
2018-01-06 03:15:36 -0500

    rewritting the basic element part

--- go101.go
+++ go101.go
@@ -16,23 +16,23 @@ import (
 	"strings"
 )
 
+var port = flag.Int("port", 55555, "server port")
+
 func main() {
 	log.SetFlags(0)
-
 	flag.Parse()
-	port := *flag.Int("port", 55555, "server port")
 
-	l, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	l, err := net.Listen("tcp", fmt.Sprintf(":%v", *port))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = openBrowser(fmt.Sprintf("http://localhost:%v", port))
+	err = openBrowser(fmt.Sprintf("http://localhost:%v", *port))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Server started: http://localhost:%v \n", port)
+	log.Printf("Server started: http://localhost:%v \n", *port)
 	(&http.Server{Handler: go101}).Serve(l)
 }
 
