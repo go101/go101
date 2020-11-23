ee7c795eba73fbf5d2954d073330cd62cbcb3d09
Go101 <22589241+go101@users.noreply.github.com>
2020-11-23 10:52:31 -0500

    small improvements

--- go-get.go
+++ go-get.go
@@ -41,8 +41,8 @@ var gogetInfos = map[string]GoGetInfo{
 	"gold": {
 		SubPackage:      "",
 		RootPackage:     "go101.org/gold",
-		GoGetSourceRepo: "https://github.com/go101/golds",
-		GoDocSourceRepo: "https://github.com/go101/golds",
+		GoGetSourceRepo: "https://github.com/go101/gold",
+		GoDocSourceRepo: "https://github.com/go101/gold",
 	},
 	"golds": {
 		SubPackage:      "",
